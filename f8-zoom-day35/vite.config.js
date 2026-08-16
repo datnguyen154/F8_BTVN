a63e9106974685b789b40c631a8487677ab226d2
datnguyen154 <xuandat1542004@gmail.com>
2026-08-16 16:03:45 +0700

    create site

--- f8-zoom-day35/vite.config.js
+++ f8-zoom-day35/vite.config.js
@@ -4,5 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/F8_BTVN/f8-zoom-day35/dist/",
+    base: "/F8_BTVN/f8-zoom-day35/",
+    build: {
+        outDir: "../site/f8-zoom-day35",
+        emptyOutDir: true,
+    },
 });
