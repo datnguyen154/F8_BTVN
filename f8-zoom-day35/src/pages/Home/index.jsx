d8835e37371e5211837d2d845cb492dce69bd305
datnguyen154 <xuandat1542004@gmail.com>
2026-08-16 16:28:42 +0700

    restyle home page

--- f8-zoom-day35/src/pages/Home/index.jsx
+++ f8-zoom-day35/src/pages/Home/index.jsx
@@ -1,31 +1,49 @@
 import { Link } from "react-router";
+import styles from "./Home.module.scss";
+
 function Home() {
     return (
-        <div>
-            <h1>Chào mừng đến với F8 React Day 35</h1>
-            <ul>
-                <li>
-                    <Link to="/counter">Counter</Link>
-                </li>
-                <li>
-                    <Link to="/todo">Todo</Link>
-                </li>
-                <li>
-                    <Link to="/profile">Profile</Link>
-                </li>
-                <li>
-                    <Link to="/products">Products</Link>
-                </li>
-                <li>
-                    <Link to="/comments">Comments</Link>
-                </li>
-                <li>
-                    <Link to="/weather">Weather</Link>
-                </li>
-                <li>
-                    <Link to="/buttons">Buttons</Link>
-                </li>
-            </ul>
+        <div className={styles.home}>
+            <div className={styles.container}>
+                <div className={styles.hero}>
+                    <h1>Chào mừng đến với F8 React Day 35</h1>
+
+                    <p>
+                        Tổng hợp các bài tập React với Vite, React Router và CSS
+                        Module.
+                    </p>
+                </div>
+
+                <ul className={styles.pageList}>
+                    <li className={styles.pageItem}>
+                        <Link to="/counter">Counter App</Link>
+                    </li>
+
+                    <li className={styles.pageItem}>
+                        <Link to="/todo">Todo List</Link>
+                    </li>
+
+                    <li className={styles.pageItem}>
+                        <Link to="/profile">Profile Card</Link>
+                    </li>
+
+                    <li className={styles.pageItem}>
+                        <Link to="/products">Product List</Link>
+                    </li>
+
+                    <li className={styles.pageItem}>
+                        <Link to="/comments">Comment System</Link>
+                    </li>
+
+                    <li className={styles.pageItem}>
+                        <Link to="/weather">Weather App</Link>
+                    </li>
+
+                    <li className={styles.pageItem}>
+                        <Link to="/buttons">Button Components</Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
