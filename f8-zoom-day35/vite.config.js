40fa77c57136af55f547e1bcfce859cef4ba34b2
datnguyen154 <xuandat1542004@gmail.com>
2026-08-16 15:29:54 +0700

    done

--- f8-zoom-day35/vite.config.js
+++ f8-zoom-day35/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/F8_BTVN/f8-zoom-day35/",
+    base: "/F8_BTVN/f8-zoom-day35/dist/",
 });
